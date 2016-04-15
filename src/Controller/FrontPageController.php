57f0e6d10ddd19bdef8f98c088d3056ad7d3a92f
Erin Marchak <erin@myplanet.com>
2016-04-15 08:07:05 -0400

    create custom theme function

--- src/Controller/FrontPageController.php
+++ src/Controller/FrontPageController.php
@@ -47,9 +47,27 @@ class FrontPageController extends ControllerBase {
    *   Return Hello string.
    */
   public function load() {
+    // Get our libraries to make this easier.
+    $storage = $this->entity_type_manager->getStorage('node');
+    $query = $storage->getQuery();
+    $view = $this->entity_type_manager->getViewBuilder('node');
+
+    // Fetch the 10 most recent nodes
+    $nids = $query
+      ->condition('status', NODE_PUBLISHED)
+      ->condition('type', 'video')
+      ->sort('created', 'DESC')
+      ->pager(10)
+      ->execute();
+    $nodes = $storage->loadMultiple($nids);
+    foreach($nodes as &$node) {
+      $node = $view->view($node, 'teaser');
+    }
+
+    // Return the nodes to the template.
     return [
-        '#type' => 'markup',
-        '#markup' => $this->t('Implement method: load')
+      '#theme' => 'fastpaced_gallery',
+      '#videos' => $nodes
     ];
   }
 
